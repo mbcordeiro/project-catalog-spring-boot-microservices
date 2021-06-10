57e5bff94bb5ebca104aa70d725a9fe39629d93c
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-06-10 13:02:35 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/projectcatalog/ProjectCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.projectcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/projectcatalog/ProjectCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.projectcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
